d668912652e8c9554b9b6ee0b1c631670602be68
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-05-21 19:38:52 +0100

    Added User List

--- src/Pages/ChatPage/Index.tsx
+++ src/Pages/ChatPage/Index.tsx
@@ -1,15 +1,29 @@
 import { useEffect, useState } from "react"
+import UserInfo from "../../SingleComponents/UserInfo"
 import './ChatUI.css'
 
 export default function(){
     const [listCompetences, setListCompetences]:any = useState([]);
+    const [listUsers, setListUsers]:any = useState([]); 
     useEffect(()=>{
+        //Ramener les Tout les competences
         fetch('http://localhost:8081/getSkills')
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             setListCompetences(data)
         })
+
+        fetch('http://localhost:8082/getAssociation')
+        .then(response => response.json())
+        .then(data => {
+            const filteredData = data.filter((item:any, index:any, self:any) => {
+                return index === self.findIndex((t:any) => (
+                    t.userEmail === item.userEmail
+                ));
+            });
+            setListUsers(filteredData);
+        });
+    
     },[])
 
     if(listCompetences)
@@ -23,6 +37,11 @@ export default function(){
               </button>
             ))}
           </div>
+          <br/><br/>
+          {listUsers.map((user:any) => (
+            <UserInfo key={user.id} image={""} name={""} email={user.userEmail} />
+          )
+          )}
         </div>
       );
     else{

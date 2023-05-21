c1dc9d80ba48ab083f65a7ba53cb6ff91792cae1
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-05-21 20:06:56 +0100

    Added Input Value

--- src/Pages/ChatPage/Index.tsx
+++ src/Pages/ChatPage/Index.tsx
@@ -4,44 +4,66 @@ import './ChatUI.css'
 
 export default function(){
     const [listCompetences, setListCompetences]:any = useState([]);
-    const [listUsers, setListUsers]:any = useState([]); 
+    const [listUsers, setListUsers]:any = useState([]);
+    const [listUsersToBeShowed, setListUsersToBeShowed]:any = useState([]); 
+    const [inputValue, setInputValue] = useState('');
     useEffect(()=>{
         //Ramener les Tout les competences
         fetch('http://localhost:8081/getSkills')
         .then(response => response.json())
         .then(data => {
+
             setListCompetences(data)
         })
 
         fetch('http://localhost:8082/getAssociation')
         .then(response => response.json())
         .then(data => {
-            const filteredData = data.filter((item:any, index:any, self:any) => {
-                return index === self.findIndex((t:any) => (
-                    t.userEmail === item.userEmail
-                ));
-            });
-            setListUsers(filteredData);
+            
+            setListUsers(data);
         });
     
     },[])
-
+    //Handle Clicking on skills and displaying users with that skill
+    const handleSkills=(e:any)=> {
+        
+        const filteredUsers = listUsers.filter((user:any)=> (user.skill||user.skillNeeded) === e.skill)
+        const filteredData = filteredUsers.filter((item:any, index:any, self:any) => {
+            return index === self.findIndex((t:any) => (
+                t.userEmail === item.userEmail
+            ));
+        });
+        setListUsersToBeShowed(filteredData)
+        
+    }
     if(listCompetences)
     return (
         <div className="competences-container">
           <h1>Competences</h1>
-          <div className="scrollable-container">
-            {listCompetences.map((competence:any) => (
-              <button key={competence.id} className="skill-button">
-                {competence.skill}
-              </button>
-            ))}
-          </div>
-          <br/><br/>
-          {listUsers.map((user:any) => (
-            <UserInfo key={user.id} image={""} name={""} email={user.userEmail} />
-          )
-          )}
+          <div className="container">
+      <div className="scrollable-container">
+        {listCompetences.map((competence:any) => (
+          <button key={competence.id} className="skill-button" onClick={() => handleSkills(competence)}>
+            {competence.skill}
+          </button>
+        ))}
+      </div>
+
+      <div className="info-container">
+        {listUsersToBeShowed.map((user:any) => (
+          <UserInfo key={user.id} image={user.image} name={user.name} email={user.userEmail} />
+        ))}
+      </div>
+
+      <input
+        className="input-dark"
+        type="text"
+        placeholder="Enter your message"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+        <button className="button-dark" >Send</button>
+    </div>
+  
         </div>
       );
     else{
